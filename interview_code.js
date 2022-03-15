094f5c69d4a807667d0d08a4c0d648f36248252a
robertlandis <16654371+robertlandis@users.noreply.github.com>
2022-03-15 00:01:29 -0500

    solution to take home project

--- interview_code.js
+++ interview_code.js
@@ -1,28 +1,59 @@
 /**
  * jQuery and underscore are already included for you if you want to use them for the project.
- * 
+ *
  * Also included are three JSON files that define three constants: routes, stops, and routeStops.
  * Those consts have all the data you'll need for this project. You can find the source files in the data directory.
  */
 
-$(function(){
-	/**
-	 * If you haven't used jQuery before, the code in here runs when the page first loads.
-	 *
-	 * As a test, let's add some HTML to the document. We'll make a simple dropdown list.
-	 * Just call the `InsertText` function with the id of the element you want to insert HTML inside of and the content to add.
-	 */
-	InsertText("mainContent", "<select><option>Option A</option><option>Option B</option>");
-
-	/**
-	 * Also, as a test, let's take a look at some of the data from the JSON files. This will log it to your browser's
-	 * Javascript console. You can see the output by opening up the dev tools in your browser.
-	 */
-	console.log({ routes });
+$(function () {
+	displayRouteDropdown();
 });
 
 //This is a helper function we're giving you to insert any HTML you want inside an element with the specified id
-function InsertText(Id, Text){
-	var Element = document.getElementById(Id);
+function InsertText(Id, Text) {
+	const Element = document.getElementById(Id);
 	Element.innerHTML = Text;
-};
+}
+
+function displayRouteDropdown() {
+	const dropdownOptions = Object.keys(routes).map((routeId) => {
+		const route = routes[routeId];
+		return `<option value='${route.routeId}'>${route.shortName} - ${route.longName}</option>`;
+	});
+	InsertText(
+		"mainContent",
+		`
+		<div>
+			<p>
+				Pick a route:
+				<select id='routeSelect' onChange='displayStopsForRoute(this)'>
+					<option value=''></option>
+					${dropdownOptions.join("")}
+				</select>
+			</p>
+			<div id='stopList'></div>
+		</div>
+	`
+	);
+}
+
+function displayStopsForRoute(routeSelect) {
+	const routeId = routeSelect.options[routeSelect.selectedIndex].value;
+	const stopsOnRoute = getStopsForRoute(routeId);
+	const stopList = stopsOnRoute.map((stopId, index) => {
+		const stop = stops[stopId];
+		return `<div>Stop ${index + 1}: ${stop.name}</div>`;
+	});
+	InsertText("stopList", stopList.join(""));
+}
+
+function getStopsForRoute(routeId) {
+	const stopIds = [];
+	Object.keys(routeStops).forEach((routeStopId) => {
+		const routeStop = routeStops[routeStopId];
+		if (routeStop.routeId == routeId) {
+			stopIds.push(routeStop.stopId);
+		}
+	});
+	return stopIds;
+}
